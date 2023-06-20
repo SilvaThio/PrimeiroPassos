a8bc634719c29ef1d994e05acad2c7477fc7a16a
SilvaThio <137129909+SilvaThio@users.noreply.github.com>
2023-06-20 19:54:18 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+    class UriMaior{
+        static void Main(){
+            int a, b, c, maior;
+
+            string[] vet = Console.ReadLine().Split(' ');
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+            c = int.Parse(vet[2]);
+
+           if(a > b)
+           {
+                maior = a; 
+           }
+           else 
+           {
+                maior = b; 
+           }
+           if (c > maior){
+              Console.WriteLine(c+" eh o maior");
+           }
+           else{
+            Console.WriteLine(maior + " eh o maior");
+           }
+           
+        }
+    }
